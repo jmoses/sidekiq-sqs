2cecf9dfe04053f716906abf5a08aff7ee5f569a
Jon Moses <jon@burningbush.us>
2012-09-27 13:12:01 -0400

    Naked gem

---
+++ lib/sidekiq-sqs.rb
@@ -0,0 +1,7 @@
+require "sidekiq-sqs/version"
+
+module Sidekiq
+  module Sqs
+    # Your code goes here...
+  end
+end
---
+++ lib/sidekiq-sqs/version.rb
@@ -0,0 +1,5 @@
+module Sidekiq
+  module Sqs
+    VERSION = "0.0.1"
+  end
+end
