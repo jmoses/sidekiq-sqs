0549e5d24eb653b460d0e7307bdcd0ead3b04ff3
Jon Moses <jon@burningbush.us>
2012-10-19 11:42:03 -0400

    I hate you

--- lib/sidekiq-sqs/util.rb
+++ lib/sidekiq-sqs/util.rb
@@ -1,7 +1,22 @@
+require 'active_support/inflector'
+
 module Sidekiq
   module Util
     def constantize(string)
-      string.constantize
+      # Try just requiring the files first
+      
+      begin
+        string.constantize
+      rescue LoadError => ex
+        begin
+          require string.split("::").map(&:underscore).join("/")
+          string.constantize
+        rescue LoadError => ex
+          STDERR.puts("Error loading constant: #{string}. #{ex.message}")
+          STDERR.puts(ex.backtrace.join("\n"))
+          raise ex
+        end
+      end
     end
   end
 end
